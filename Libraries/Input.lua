9f9e9dfaaf6021fc9d38bbc6b56ab11c144c34a9
jaeelin <asc5ndant@example.com>
2026-06-14 02:48:00 -0400

    input test

--- Libraries/Input.lua
+++ Libraries/Input.lua
@@ -72,7 +72,16 @@ function Input.Bind(WindowFunctions: {}, Context: {})
 		if not Context.can_drag or not dragging or not target_position then return end
 		
 		if Context.maximized then
-			WindowFunctions:Maximize(target_position)
+			Context.maximized = false
+
+			Context.Background.Size = UDim2.new(0.4, 0, 0.55, 0)
+
+			local mouse = UserInputService:GetMouseLocation()
+
+			target_position = UDim2.fromOffset(
+				mouse.X - Context.Background.AbsoluteSize.X * 0.5,
+				mouse.Y - 20
+			)
 		end
 		
 		Context.Background.Position = Context.Background.Position:Lerp(target_position, 0.35)
