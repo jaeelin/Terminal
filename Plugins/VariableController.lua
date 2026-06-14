befd9940f807ad8681ceb413cbef1ce69e4dce2c
jaeelin <asc5ndant@example.com>
2026-06-13 21:44:27 -0400

    god

--- Plugins/VariableController.lua
+++ Plugins/VariableController.lua
@@ -1,19 +1,25 @@
+print("hello")
+
 local Players = cloneref and cloneref(game:GetService("Players")) or game:GetService("Players")
+
+print("hello2")
 
 local LocalPlayer = Players.LocalPlayer
 
+print("hello3")
+
 return function(WindowFunctions: {}, PrintLine: () -> (), Directory: string, PluginContext: {})
-    print("PluginContext:", PluginContext)
+	print("PluginContext:", PluginContext)
 
-    PluginContext.System = {
-        Username = LocalPlayer.Name,
-        Hostname = "DESKTOP-4K8P7A2",
+	PluginContext.System = {
+		Username = LocalPlayer.Name,
+		Hostname = "DESKTOP-4K8P7A2",
 
-        Ip = "192.168.1.104",
-        MacAddress = "00-1A-2B-3C-4D-5E",
+		Ip = "192.168.1.104",
+		MacAddress = "00-1A-2B-3C-4D-5E",
 
-        Os = string.format("Calamari [Version %s]", WindowFunctions.Version or "unknown"),
+		Os = string.format("Calamari [Version %s]", WindowFunctions.Version or "unknown"),
 
-        BootTime = os.time(),
-    }
+		BootTime = os.time(),
+	}
 end
